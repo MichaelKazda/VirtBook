f75e61152bf858db27daf0c1b33c4872ce67a8b2
Michael Kazda <michaelkazda@seznam.cz>
2021-01-26 23:47:15 +0100

    Saving notification token

--- app/src/main/java/com/example/virtbook/MainActivity.kt
+++ app/src/main/java/com/example/virtbook/MainActivity.kt
@@ -1,31 +1,30 @@
 package com.example.virtbook
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.anychart.AnyChart
-import com.anychart.AnyChartView
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.example.virtbook.services.DataHandler
 import com.onesignal.OneSignal
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val dataHandler = DataHandler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // OneSignal notification logs
+        /* ONESIGNAL NOTIFICATION SETTING*/
+        // Notification logs
         OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
-        // OneSignal Initialization
+        // Initialization
         OneSignal.startInit(this)
             .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
             .unsubscribeWhenNotificationsAreDisabled(true)
             .init()
+        // Getting notificationToken and saving into DB
+        val notifToken = OneSignal.getPermissionSubscriptionState().subscriptionStatus.userId
+        dataHandler.saveNotifToken(notifToken)
 
         MyApp.bookID="EA45I"
     }
--- app/src/main/java/com/example/virtbook/services/DataHandler.kt
+++ app/src/main/java/com/example/virtbook/services/DataHandler.kt
@@ -1,12 +1,18 @@
 package com.example.virtbook.services
 
+import android.util.Log
+import com.example.virtbook.MyApp
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 /**
  * Class for handling data, but not accessing them
  */
 class DataHandler {
+
+    private val db = Firebase.firestore
 
     // Returns data maps for garage site
     fun getDataGarage(docRef: DocumentSnapshot, data: String ): Map<*,*> {
@@ -21,6 +27,15 @@ class DataHandler {
             else -> {
                 userData
             }
+        }
+    }
+
+    // Saves notificationToken to DB
+    fun saveNotifToken(notifToken: String) {
+        try {
+            db.collection("users").document(MyApp.userID).update("notificationToken", notifToken)
+        }catch (e: Exception){
+            Log.e("SavingNotifToken Err", notifToken)
         }
     }
 
